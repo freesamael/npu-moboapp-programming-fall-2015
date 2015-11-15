6ae28a9957f83d79cd1d98eae7f5af7e5bd0d3e8
Samael Wang <freesamael@gmail.com>
2015-11-15 16:22:41 +0800

    Add Firefox OS sample.

---
+++ ubike-fxos/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+window.addEventListener("load", sync);
+
+function sync() {
+  document.getElementById("ubike-list").innerHTML = '';
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", () => updateUBikeList(JSON.parse(xhr.responseText)));
+  xhr.open("GET", "http://data-taipei.blackbing.net/youbike");
+  xhr.send();  
+}
+
+function updateUBikeList(response) {
+  var stations = response.retVal;
+  
+  var list = document.getElementById("ubike-list");
+  for (var sid in stations) {    
+    // create DOM nodes
+    var listItem = document.createElement("a");
+    var listItemDiv = document.createElement("div");
+    var listItemSiteName = document.createElement("h3");    
+    var listItemBikes = document.createElement("p");
+    listItemDiv.appendChild(listItemSiteName);
+    listItemDiv.appendChild(listItemBikes);
+    listItem.appendChild(listItemDiv);
+    list.appendChild(listItem);
+    
+    // fill values
+    listItemSiteName.innerHTML = stations[sid].sna;
+    listItemBikes.innerHTML = 'Bikes: ' + stations[sid].sbi + ' / ' + stations[sid].tot;
+  }
+}
